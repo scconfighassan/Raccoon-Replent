a84e98e5e8c0166341dde4a3adec257de2d7ac7e
Hassan Mahdi <hassanmahdi96.hm@gmail.com>
2018-06-29 17:46:40 -0400

    Fixed speaker
    turning off and on instantnously SOLVED

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,10 @@ int echoPin = 12;
 //Second
 int trigPin2 = 10;
 int echoPin2 = 9;
-long firstSen, secondSen;
 int speaker = 8;
 
 void frequencyDistance(long cm1,long cm2, int speakerInput)
 {
-
   if (cm2<20&&cm1>20){
       tone(speakerInput,(100+cm2)*100);
     }
@@ -25,7 +23,8 @@ void frequencyDistance(long cm1,long cm2, int speakerInput)
   }
 
 }
-void sensors(int trig, int echo, long distance){
+int sensors(int trig, int echo){
+  long distance = 0;
   digitalWrite(trig, LOW);
   delayMicroseconds(5);
   digitalWrite(trig, HIGH);
@@ -33,7 +32,7 @@ void sensors(int trig, int echo, long distance){
   digitalWrite(trig, LOW);
 
   long duration = pulseIn(echo, HIGH);
-  distance = (duration/2) / 29.1;
+  return distance = (duration/2) / 29.1;
 }
 void setup() {
       //Serial Port begin
@@ -47,10 +46,8 @@ void setup() {
   pinMode(echoPin2, INPUT);
 }
 void loop()
-{
-
-
-sensors(trigPin2,echoPin2,secondSen);
-sensors(trigPin,echoPin,firstSen);
-frequencyDistance(firstSen,secondSen,speaker);
-}
+  {
+    long d1= sensors(trigPin2,echoPin2);
+    long d2= sensors(trigPin,echoPin);
+    frequencyDistance(d1,d2,speaker);
+  }
