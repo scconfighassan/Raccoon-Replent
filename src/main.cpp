9ea0aeb4010467056592c374f6112cbaa0fbe323
Hassan Mahdi <hassanmahdi96.hm@gmail.com>
2018-07-12 13:19:26 -0400

    Commenting

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
 Author: Hassan Mahdi
 Date:
-Description: Raccooon Repellent based on Sound Frequency.
+Description: Raccooon Repellent based on Sound Frequency. Using 5 ultrasonic sensors
 */
 #include <Arduino.h>
 //First Ultrasonic Sensor
@@ -21,48 +21,59 @@ int trigPin5 = 3; // brown
 int echoPin5 = 2;
 //speakerInput
 int speaker = 8;
-
+/*
+plugs in sensor-detected distances thus triggers the speakers with a SPECIFIC Frequency.
+(Frequency subject to change)
+*/
 void frequencyDistance(long cm1,long cm2, long cm3, long cm4, long cm5, int speakerInput1)
-{
+{ /*
   if (cm2<20&&cm1>20&&cm3>20&&cm4>20&&cm5>20){
-      tone(speakerInput1,(10000));
+      tone(speakerInput1,(15000));
 
     }
 
   if (cm1<20&&cm2>20&&cm3>20&&cm4>20&&cm5>20){
-    tone(speakerInput1,10000);
+    tone(speakerInput1,15000);
 
   }
   if (cm3<20&&cm2>20&&cm1>20&&cm4>20&&cm5>20){
-    tone(speakerInput1,10000);
+    tone(speakerInput1,15000);
 
   }
   if (cm4<20&&cm2>20&&cm3>20&&cm1>20&&cm5>20){
-    tone(speakerInput1,10000);
+    tone(speakerInput1,15000);
 
   }
   if (cm5<20&&cm2>20&&cm3>20&&cm1>20&&cm4>20){
-    tone(speakerInput1,10000);
+    tone(speakerInput1,15000);
 
   }
+  */
   if ((cm1>20)&&(cm2>20)&&(cm3>20)&&(cm4>20)&&(cm5>20)){
 
     noTone(speakerInput1);
-
-
   }
 
+  else{
+  tone(speakerInput1,10000);
+  }
 }
+/*
+Ultrasonic Sensor HC-SR04
+The sensor will emit a Frequency of 40k Hz. If there is an object on its path it will bounce back or get reflected
+back to the sensor. You can calculate the distance by considering speed of sound and travel time.
+speed of sound 340 m/s
+*/
 int sensors(int trig, int echo){
   long distance = 0;
   digitalWrite(trig, LOW);
-  delayMicroseconds(5);
+  delayMicroseconds(2);
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
 
-  long duration = pulseIn(echo, HIGH);
-  return distance = (duration/2) / 29.1;
+  long duration = pulseIn(echo, HIGH); //bounce sound wave
+  return distance = (duration/2) / 29.1; // distance in cm.
 }
 void setup() {
       //Serial Port begin
